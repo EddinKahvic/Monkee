38b54c4b57139e97f688021d02e8457fb2d4314b
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2024-08-04 02:55:21 +0200

    add CommandBuilder class

--- src/types/command.types.ts
+++ src/types/command.types.ts
@@ -1,6 +1,23 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js'
+import {
+  SlashCommandBuilder,
+  ChatInputCommandInteraction,
+  SlashCommandOptionsOnlyBuilder,
+} from 'discord.js'
+
+export type SlashCommandBuilderLike =
+  | SlashCommandBuilder
+  | SlashCommandOptionsOnlyBuilder
+
+export interface CommandInformation {
+  description?: string
+}
+
+export type CommandExecutable = (
+  interaction: ChatInputCommandInteraction
+) => Promise<void>
 
 export interface Command {
-  data: SlashCommandBuilder
-  execute: (interaction: ChatInputCommandInteraction) => Promise<void>
+  data: SlashCommandBuilderLike
+  information?: CommandInformation
+  execute: CommandExecutable
 }
