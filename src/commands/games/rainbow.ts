cad26e53124664ad09f10322d08a6d58d4772560
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2024-08-07 05:56:08 +0200

    update command to make select operator

--- src/commands/games/rainbow.ts
+++ src/commands/games/rainbow.ts
@@ -1,12 +1,57 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js'
+import {
+  AttachmentBuilder,
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from 'discord.js'
 import CommandBuilder from '~/classes/command.classes'
+import {
+  createOperatorIconBuffer,
+  getRandomOperator,
+} from '~/helpers/rainbow.helpers'
+
+const sides = [
+  { name: 'Attacking', value: 'attacking' },
+  { name: 'Defense', value: 'defence' },
+]
 
 const data = new SlashCommandBuilder()
   .setName('rainbow')
   .setDescription('Picks a random operator for you')
+  .addStringOption((option) =>
+    option
+      .setName('side')
+      .setDescription('Attacker or defender side')
+      .setRequired(true)
+      .addChoices(sides)
+  )
 
 async function execute(interaction: ChatInputCommandInteraction) {
-  await interaction.reply(`Rainbow Six Siege Operator`)
+  const side = interaction.options.getString('side')
+
+  if (!side) {
+    await interaction.reply('A side is required')
+    return
+  }
+
+  const operator = getRandomOperator(side)
+
+  const iconBuffer = await createOperatorIconBuffer(operator)
+
+  if (!iconBuffer) {
+    await interaction.reply('Something went wrong when picking an operator')
+    return
+  }
+
+  const iconName = `${operator.id}.png`
+  const icon = new AttachmentBuilder(iconBuffer, { name: iconName })
+  const embed = new EmbedBuilder()
+    .setColor(0x729c7c)
+    .setTitle(operator.name)
+    .setDescription(`${interaction.user} is playing as ${operator.name}!`)
+    .setImage(`attachment://${iconName}`)
+
+  await interaction.reply({ embeds: [embed], files: [icon] })
 }
 
 export default new CommandBuilder()
