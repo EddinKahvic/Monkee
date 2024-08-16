630ea865b1b9158e0fe2c76b5af363e361b4f67d
EddinKahvic <eddin.kahvic@hotmail.com>
2024-08-16 23:33:50 +0200

    Removed author icon, name and timestamp

--- src/commands/utility/help.ts
+++ src/commands/utility/help.ts
@@ -7,28 +7,24 @@ import {
 import CommandBuilder from '~/classes/command.classes'
 import { getCommands } from '~/helpers/commands.helper'
 
-const image = new AttachmentBuilder('assets/monkee.png')
-
 const noDescription = 'This command has no description'
 
-const commands = getCommands().map((command) => ({
+const commands = getCommands().map(command => ({
   name: `/${command.data.name}`,
   value: `\`${command.information?.description ?? noDescription}\``,
 }))
 
 const embed = new EmbedBuilder()
   .setColor(0x729c7c)
-  .setAuthor({ name: 'Monkee', iconURL: 'attachment://monkee.png' })
   .setTitle('List of commands')
   .addFields(commands)
-  .setTimestamp()
 
 const data = new SlashCommandBuilder()
   .setName('help')
   .setDescription('Displays a descriptive list of all commands')
 
 async function execute(interaction: ChatInputCommandInteraction) {
-  await interaction.reply({ embeds: [embed], files: [image] })
+  await interaction.reply({ embeds: [embed] })
 }
 
 export default new CommandBuilder().setData(data).setExecutable(execute)
