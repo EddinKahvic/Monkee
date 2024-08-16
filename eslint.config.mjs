7f3bdf45b411cf0b9fb11a529bf1bb8d7f4c8ee3
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2024-08-17 00:27:25 +0200

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,10 @@ import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.recommended
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  }
 )
